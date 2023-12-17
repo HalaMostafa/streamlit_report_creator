573b0faf1edc0cf0d6e2f2c2d0397472ddc36c74
Hala Mostafa <hala.eng2014@gmail.com>
2023-12-17 16:12:13 +0200

    refactor:

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import helpers.input_helpers as input_helpers
 
-st.write("Hello")
+def main_app():
+    file = input_helpers.fileUploader()
+    df = input_helpers.getDataFrame(file)
+    st.write(df.head())
+
+main_app()
