dddc9cc94c698393f88af8d651875c7b0cf50021
Hala Mostafa <hala.eng2014@gmail.com>
2023-12-18 17:14:16 +0200

    feat(app.py): :sparkles: add side bar

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import helpers.input_helpers as input_helpers
 
-def main_app():
-    file = input_helpers.fileUploader()
-    df = input_helpers.getDataFrame(file)
-    st.write(df.head())
 
-main_app()
+def main():
+
+    with st.sidebar:
+        st.header("Welcom To My Reports")
+        uploaded_file = input_helpers.fileUploader()
+        if uploaded_file is not None:
+            st.header("ADD")
+            table = st.button("Table")
+            chart = st.button("Chart")
+
+
+if __name__ == "__main__":
+    main()
