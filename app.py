93d69a1b33a479072bc6d04631352a114b3223d6
Hala Mostafa <hala.eng2014@gmail.com>
2023-12-20 17:38:22 +0200

    feat(app.py): :construction: select box render first option always None

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 import streamlit as st
+import pandas as pd
 import helpers.input_helpers as input_helpers
+import helpers.analysis_helpers as analysis_helpers
 
+     
 
-def main():
-
+def sideBar():
+        
     with st.sidebar:
         st.header("Welcom To My Reports")
         uploaded_file = input_helpers.fileUploader()
-        if uploaded_file is not None:
-            st.header("ADD")
-            table = st.button("Table")
-            chart = st.button("Chart")
+        analysis_helpers.getDataframe(uploaded_file)
+
+def main():
+    option = None # Initialize selected_option outside the sidebar
+    sideBar()
+    try:
+        st.write(df.head())
+        if 'click_count' not in st.session_state:
+            st.session_state.click_count = 0
+        if 'click_history' not in st.session_state:
+            st.session_state.click_history = []
+
+        if st.button('Add Analysis'):
+            st.session_state.click_count += 1
+            st.session_state.click_history.append(f'Button clicked {st.session_state.click_count} times')
+
+        for _ in st.session_state.click_history:
+            with st.sidebar:
+                option = st.selectbox("Select Analysis",[None,'one level pivot table','two level pivot table'])
+            if option is not None:
+                st.write('You Selected',option) 
+    except NameError:
+        print("waiting for data to be uploaded!")
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
