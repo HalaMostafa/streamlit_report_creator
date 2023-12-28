3e1d2e019de76b86b2f030f33689e80b4c1dc30d
Hala Mostafa <hala.eng2014@gmail.com>
2023-12-28 15:30:24 +0200

    feat(analysis_helper.py): :sparkles: time series analysis

--- helpers/analysis_helpers.py
+++ helpers/analysis_helpers.py
@@ -41,3 +41,20 @@ def pivot_table_analysis(df,index_columns:list,columns:list,value:str, agg_func:
     """
     return pd.pivot_table(df, values=value, index=index_columns,
                        columns=columns, aggfunc=agg_func)
+
+def time_series_analysis(df, date_column: list, groupby_period: str, **kwargs):
+    """return one level group table with, take a datetime column and convert it into periods in which get the periond this date belongs to
+    Args:
+    -df: pandas Dataframe(requiered).
+    -date_column: (string) name of date column to group with (requiered).
+    -groupby_period: (string) one of [y,q,m,D] (requiered).
+    -kwargs: dict like with values refer to aggregation function and values refer to columns to aggregate(required)
+    returns:
+    one level grouppped table with date period index
+    """
+    aggrigation_dictionary = {}
+    for key, value in kwargs.items():
+        aggrigation_dictionary.update(**{v: str(key) for v in value})
+    df[date_column] = df[date_column].apply(pd.to_datetime())
+    periods = df[date_column].dt.to_period(groupby_period).astype(str)
+    return df.groupby(periods).agg(aggrigation_dictionary)
